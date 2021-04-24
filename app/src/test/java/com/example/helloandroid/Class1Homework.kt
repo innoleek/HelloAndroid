bd3724d57210345e7d38b3f70e972be4704f0d64
Min Lee <minlee1@microsoft.com>
2021-04-24 15:17:44 -0700

    Initial Commit

---
+++ app/src/test/java/com/example/helloandroid/Class1Homework.kt
@@ -0,0 +1,46 @@
+package com.example.helloandroid
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class Class1Homework {
+    @Test
+    fun `Task 1 - Print sentences`() {
+        /*
+        Print below sentences
+
+        HelloAndroid!
+        I like summer
+         */
+        var input = ""
+        var output = ""
+
+        assertEquals("A", "a")
+    }
+
+    @Test
+    fun `Task 2 - Combine strings`() {
+        /*
+        Using below 5 variables, print "I like chicken pasta and chicken curry."
+         */
+        var input1 = "chicken "
+        var input2 = "and "
+        var input3 = "curry."
+        var input4 = "pasta "
+        var input5 = "I like "
+    }
+
+    @Test
+    fun `Task 3 - String replace`() {
+        /*
+        Using below 5 variables, print "I like chicken pasta and chicken curry."
+         */
+        var input = "I hvad ppapp great davpy"
+    }
+}
---
+++ helloandroidlibrary/src/test/java/com/example/heeloandroid/library/Class1HomeworkAnswer.kt
@@ -0,0 +1,6 @@
+package com.example.heeloandroid.library
+
+internal fun task1Answer(): String {
+    return "HelloAndroid!\nI like summer";
+}
+
